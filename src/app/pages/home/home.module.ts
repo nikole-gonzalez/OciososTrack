d0855096ff95119a5274187258538865502afa05
NikoleGonzalez <niko.gonzalez@duocuc.cl>
2024-10-17 23:05:16 -0300

    componente tab again

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,7 +4,11 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
+
 import { HomePageRoutingModule } from './home-routing.module';
+import { SugerenciasComponent } from 'src/app/components/sugerencias/sugerencias.component';
+import { UsuarioComponent } from 'src/app/components/usuario/usuario.component';
+import { ConfiguracionComponent } from 'src/app/components/configuracion/configuracion.component';
 
 
 @NgModule({
@@ -12,8 +16,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
+    HomePageRoutingModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, SugerenciasComponent, UsuarioComponent, ConfiguracionComponent]
 })
 export class HomePageModule {}
