2030e9e7b3032710eee2faf4c6157172853230c9
Constanza Zúñiga <cons.zunigap@duocuc.cl>
2024-11-30 18:13:26 -0300

    Detalles

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ociosos1',
+  appId: 'ociosos.track.proyecto.com',
+  appName: 'OciososTrack',
   webDir: 'www'
 };
 
